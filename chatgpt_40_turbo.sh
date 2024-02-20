38894064209712b74caeda341d5a7fedf1096a83
Takuma Ikeda <takuma.ikeda@eeeeg.tokyo>
2024-02-20 12:53:53 +0900

    fix: prompt を変数化

--- chatgpt_40_turbo.sh
+++ chatgpt_40_turbo.sh
@@ -2,4 +2,4 @@
 
 source ./vars.sh
 
-cat "$en_dir/$en_file" | llm -m 4-turbo -s "この英語の文章を日本語に翻訳してください。翻訳する際、日本語の句読点や読点のあとに改行を入れて見やすくして下さい。" > "$ja_dir/$ja_file"
+cat "$en_dir/$en_file" | llm -m 4-turbo -s "$prompt" > "$ja_dir/$ja_file"
